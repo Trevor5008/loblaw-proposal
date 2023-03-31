ce63d74f62dae98d88243875cf26064b752535e1
mickealajp <124074359+mickealajp@users.noreply.github.com>
2023-03-31 00:48:22 -0400

    adds search bar and styling to navigation area

--- src/App.js
+++ src/App.js
@@ -9,6 +9,52 @@ import Form from './components/Form/Form';
 import './App.scss';
 import Image from 'mui-image';
 import bob from './assets/logos/bob_loblaw_icon.png';
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import Header from './components/Header/Header';
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 
 function App() {
@@ -16,16 +62,16 @@ function App() {
      <div className="App">
        <Box className="navBox">
          <AppBar position="static">
-           <Toolbar className='navBox__toolbar'>
+           <Toolbar className="navBox__toolbar">
              <IconButton
                size="large"
                edge="start"
                color="inherit"
                aria-label="menu"
-               className='navBox__btn'
+               className="navBox__btn"
              >
-            <MenuIcon />
-             <Image src={bob} className='navBox__logo' />
+               <MenuIcon />
+               <Image src={bob} className="navBox__logo" duration={0} />
              </IconButton>
              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                News
@@ -33,7 +79,18 @@ function App() {
              <Button color="inherit">Login</Button>
            </Toolbar>
          </AppBar>
+         <Header />
        </Box>
+       <Search id="search">
+         <SearchIconWrapper className="nav__search-icon">
+           <SearchIcon />
+         </SearchIconWrapper>
+         <StyledInputBase
+           className="nav__search-input"
+           placeholder="Search for product"
+           inputProps={{ "aria-label": "search" }}
+         />
+       </Search>
 
        {/* <Form /> */}
      </div>
