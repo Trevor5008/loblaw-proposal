367f27ccdf489b45a617b207adac17a416f6c4c9
mickealajp <124074359+mickealajp@users.noreply.github.com>
2023-03-31 09:42:51 -0400

    adds test value for preferences component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import Header from './components/Header/Header';
+import Preferences from './components/Preferences/Preferences';
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -91,6 +92,7 @@ function App() {
            inputProps={{ "aria-label": "search" }}
          />
        </Search>
+
       <h1 className="welcome__text">Welcome You!</h1>
        {/* <Form /> */}
        <section className="banner">
